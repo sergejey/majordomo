90a4ceb5abf7fd23ed1a2c692673c823b7af5664
Sergei Jeihala <jey@tut.by>
2014-07-12 17:35:27 +0300

    exec function fixed (windows)

--- lib/common.class.php
+++ lib/common.class.php
@@ -649,10 +649,12 @@
  function execInBackground($cmd) {
     if (substr(php_uname(), 0, 7) == "Windows"){
         //pclose(popen("start /B ". $cmd, "r")); 
-
          try {
-          $WshShell = new COM("WScript.Shell");
-          $oExec = $WshShell->Run("cmd /C ".$cmd, 0, false);
+          //pclose(popen("start /B ". $cmd, "r")); 
+          system($cmd);
+          //$WshShell = new COM("WScript.Shell");
+          //$oExec = $WshShell->Run("cmd /C ".$cmd, 0, false);
+          //exec($cmd);
          } catch(Exception $e){
           DebMes('Error: exception '.get_class($e).', '.$e->getMessage().'.');
          }
