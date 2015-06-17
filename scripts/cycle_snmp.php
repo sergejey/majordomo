351c394bfc48f5eb42edf14f396bef4deeeb6db7
LDV <palacex@gmail.com>
2015-06-17 17:39:46 +0300

    add: code style support

--- scripts/cycle_snmp.php
+++ scripts/cycle_snmp.php
@@ -9,7 +9,7 @@ include_once("./lib/threads.php");
 set_time_limit(0);
 
 // connecting to database
-$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); 
+$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
 
 include_once("./load_settings.php");
 include_once(DIR_MODULES . "control_modules/control_modules.class.php");
@@ -20,19 +20,21 @@ include_once(DIR_MODULES . 'snmpdevices/snmpdevices.class.php');
 
 $snmpdevices = new snmpdevices();
 
-$tmp=SQLSelectOne("SELECT ID FROM snmpdevices LIMIT 1");
-if (!$tmp['ID']) {
- exit; // no devices added -- no need to run this cycle
-}
+$tmp = SQLSelectOne("SELECT ID FROM snmpdevices LIMIT 1");
 
-echo date("H:i:s") . " running " . basename(__FILE__) . "\n";
-while(1) 
+if (!$tmp['ID'])
+   exit; // no devices added -- no need to run this cycle
+
+echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
+
+while (1)
 {
-   setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
+   setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+   
    // check all web vars
-   $snmpdevices->readAll(); 
+   $snmpdevices->readAll();
 
-   if (file_exists('./reboot') || $_GET['onetime']) 
+   if (file_exists('./reboot') || $_GET['onetime'])
    {
       $db->Disconnect();
       exit;
@@ -42,5 +44,3 @@ while(1)
 }
 
 DebMes("Unexpected close of cycle: " . basename(__FILE__));
-
-?>
