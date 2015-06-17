395d85df16f52ad90d3e64172a57185fb570efee
LDV <palacex@gmail.com>
2015-06-17 14:06:28 +0300

    add: code style support

--- scripts/sample_cycle_arduino_direct.php
+++ scripts/sample_cycle_arduino_direct.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * User: DnAp
  * Date: 12.06.13
@@ -13,10 +14,9 @@ include_once("./config.php");
 include_once("./lib/loader.php");
 include_once("./lib/threads.php");
 
-include_once(__DIR__.'/php_serial.class.php');
+include_once(__DIR__ . '/php_serial.class.php');
 
 set_time_limit(0);
-
 
 // connecting to database
 $db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
@@ -26,60 +26,84 @@ include_once(DIR_MODULES . "control_modules/control_modules.class.php");
 
 $ctl = new control_modules();
 
-$handle = @opendir('/dev/');
-if (!$handle){
-    DebMes("Support only *nux system" . basename(__FILE__));
-    exit;
+$handle = opendir('/dev/');
+
+if (!$handle)
+{
+   DebMes("Support only *nux system" . basename(__FILE__));
+   exit;
 }
 
 $tty = array();
-while (false !== ($entry = readdir($handle))) {
-    if(substr($entry, 0, 6) == 'ttyACM'){
-        $tty[] = substr($entry, 6);
-    }
+
+while (false !== ($entry = readdir($handle)))
+{
+   if(substr($entry, 0, 6) == 'ttyACM')
+      $tty[] = substr($entry, 6);
+}
+
+if (empty($tty))
+{
+   DebMes("/dev/ttyACM* not found" . basename(__FILE__));
+   exit;
 }
 
 sort($tty);
-if(empty($tty)) {
-    DebMes("/dev/ttyACM* not found" . basename(__FILE__));
-    exit;
-}
 $updated_time = time();
-try{
-    $serial = new PhpSerial;
-    $serial->deviceSet("/dev/ttyACM".end($tty));
-    $serial->confBaudRate(9600);
-    $serial->confParity("none");
-    $serial->confCharacterLength(8);
-    $serial->confStopBits(1);
-    $data = "";
-    if($serial->deviceOpen()){
-        DebMes("Open device ".end($tty).": " . basename(__FILE__));
-        do{
-            $data .= $serial->readPort();
 
-            //GET /objects/?object=sensorGarage&op=m&m=statusChanged&status=%i HTTP/1.0
-            $start = strpos($data, 'GET ');
-            $end = strpos($data, ' HTTP/1.0');
-            if($start !== false && $end) {
-                $url = BASE_URL.trim(substr($data, $start+4, $end-$start-4));
-                $context  = stream_context_create(array('http' => array('timeout' => 5)));
-                file_get_contents($url, false, $context);
-                $data = substr($data, $end+9);
-            }
-            if (!$updated_time || (time() - $updated_time) > 1 * 60 * 60)
-            {
-                //Log activity every hour
-                DebMes("Cycle running OK: " . basename(__FILE__));
-                $updated_time = time();
-            }
-            usleep($uSleep);
-        }while(!file_exists('./reboot') || $_GET['onetime']);
-    }else{
-        DebMes("Not open device ".end($tty).": " . basename(__FILE__));
-    }
-}catch (Exception $e){
-    DebMes("Not open device ".end($tty)." ".$e->getMessage().": " . basename(__FILE__));
+try
+{
+   $serial = new PhpSerial;
+   $serial->deviceSet("/dev/ttyACM" . end($tty));
+   $serial->confBaudRate(9600);
+   $serial->confParity("none");
+   $serial->confCharacterLength(8);
+   $serial->confStopBits(1);
+   $data = "";
+   
+   if ($serial->deviceOpen())
+   {
+      DebMes("Open device " . end($tty) . ": " . basename(__FILE__));
+
+      do
+      {
+         $data .= $serial->readPort();
+
+         //GET /objects/?object=sensorGarage&op=m&m=statusChanged&status=%i HTTP/1.0
+         $start = strpos($data, 'GET ');
+         $end = strpos($data, ' HTTP/1.0');
+         
+         if ($start !== false && $end)
+         {
+            $url = BASE_URL . trim(substr($data, $start + 4, $end - $start - 4));
+            $context  = stream_context_create(array('http' => array('timeout' => 5)));
+
+            file_get_contents($url, false, $context);
+
+            $data = substr($data, $end + 9);
+         }
+
+         if (!$updated_time || (time() - $updated_time) > 1 * 60 * 60)
+         {
+            //Log activity every hour
+            DebMes("Cycle running OK: " . basename(__FILE__));
+            $updated_time = time();
+         }
+         
+         usleep($uSleep);
+      }
+      while(!file_exists('./reboot') || $_GET['onetime']);
+   }
+   else
+   {
+      DebMes("Not open device " . end($tty) . ": " . basename(__FILE__));
+   }
 }
-$db->Disconnect();
+catch (Exception $e)
+{
+   DebMes("Not open device " . end($tty) . " " . $e->getMessage() . ": " . basename(__FILE__));
+}
+
 DebMes("Unexpected close of cycle: " . basename(__FILE__));
+
+$db->Disconnect();
