8d996e68b5145bdf97d3573d65c58a5d2de1a54a
Sergei Jeihala <jey@tut.by>
2015-10-29 15:49:24 +0300

    Websockets fixes

--- cycle.php
+++ cycle.php
@@ -158,8 +158,12 @@ if (!is_array($restart_threads))
                          'cycle_ping.php',
                          'cycle_scheduler.php',
                          'cycle_states.php',
-                         'cycle_websockets.php',
                          'cycle_webvars.php');
+
+ if (!defined('DISABLE_WEBSOCKETS') || DISABLE_WEBSOCKETS==0) {
+  $restart_threads[]='cycle_websockets.php';
+ }
+
 }
 
 while (false !== ($result = $threads->iteration()))
