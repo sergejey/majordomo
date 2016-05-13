a90fed28484ca8c0f5dc3c3e0cd608d829763762
Dmitriy Studinskiy <dms@smartling.com>
2016-05-13 18:38:53 +0300

    Fixed signature of saveParams in successor classes

--- modules/dashboard/dashboard.class.php
+++ modules/dashboard/dashboard.class.php
@@ -31,21 +31,21 @@ function dashboard() {
 *
 * @access public
 */
-function saveParams() {
- $p=array();
+function saveParams($data=1) {
+ $data=array();
  if (IsSet($this->id)) {
-  $p["id"]=$this->id;
+  $data["id"]=$this->id;
  }
  if (IsSet($this->view_mode)) {
-  $p["view_mode"]=$this->view_mode;
+  $data["view_mode"]=$this->view_mode;
  }
  if (IsSet($this->edit_mode)) {
-  $p["edit_mode"]=$this->edit_mode;
+  $data["edit_mode"]=$this->edit_mode;
  }
  if (IsSet($this->tab)) {
-  $p["tab"]=$this->tab;
+  $data["tab"]=$this->tab;
  }
- return parent::saveParams($p);
+ return parent::saveParams($data);
 }
 /**
 * getParams
