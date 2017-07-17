b8740f49e42906591e340d9a16b85396f02223d2
tarasfrompir <tarasfrompir@inbox.ru>
2017-07-17 17:17:08 +0300

    bluetooth module added

---
+++ modules/btdevices/btdevices.class.php
@@ -0,0 +1,257 @@
+<?php
+/**
+* btdevices 
+*
+* btdevices
+*
+* @package MajorDoMo
+* @author Serge Dzheigalo <jey@tut.by> http://smartliving.ru/
+* @version 0.3 (wizard, 15:03:07 [Mar 27, 2009])
+*/
+//
+//
+class btdevices extends module {
+/**
+* btdevices
+*
+* Module class constructor
+*
+* @access private
+*/
+function btdevices() {
+  $this->name="btdevices";
+  $this->title="<#LANG_MODULE_BT_DEVICES#>";
+  $this->module_category="<#LANG_SECTION_DEVICES#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams() {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams() {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $out['TAB']=$this->tab;
+  if ($this->single_rec) {
+   $out['SINGLE_REC']=1;
+  }
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+ if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
+  $out['SET_DATASOURCE']=1;
+ }
+ if ($this->data_source=='btdevices' || $this->data_source=='') {
+
+ if ($this->mode=='delete_unknown') {
+  SQLExec("DELETE FROM btdevices WHERE USER_ID=0");
+  $this->redirect("?");
+ }
+
+ if ($this->mode=='delete_once') {
+  SQLExec("DELETE FROM btdevices WHERE USER_ID=0 AND TO_DAYS(FIRST_FOUND)=TO_DAYS(LAST_FOUND)");
+  $this->redirect("?");
+ }
+
+
+  if ($this->view_mode=='' || $this->view_mode=='search_btdevices') {
+   $this->search_btdevices($out);
+  }
+  if ($this->view_mode=='edit_btdevices') {
+   global $id;
+   $this->edit_btdevices($out, $id);
+  }
+
+  if ($this->view_mode=='delete_btdevices') {
+   $this->delete_btdevices($this->id);
+   $this->redirect("?");
+  }
+
+
+
+ }
+}
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ global $session;
+
+}
+/**
+* btdevices search
+*
+* @access public
+*/
+ function search_btdevices(&$out) {
+
+  $btdevices=SQLSelect("SELECT btdevices.*, users.NAME FROM btdevices LEFT JOIN users ON btdevices.USER_ID=users.ID ORDER BY LAST_FOUND DESC");
+  if ($btdevices[0]['ID']) {
+   $out['DEVICES']=$btdevices;
+  }
+
+ }
+/**
+* btdevices edit/add
+*
+* @access public
+*/
+ function edit_btdevices(&$out, $id) {
+  $rec=SQLSelectOne("SELECT * FROM btdevices WHERE ID='".(int)$id."'");
+  if ($this->mode=='update') {
+   global $title;
+   global $user_id;
+   $rec['TITLE']=$title;
+   $rec['USER_ID']=$user_id;
+   SQLUpdate('btdevices', $rec);
+   $this->redirect("?");
+  }
+  $rec['LOG']=nl2br($rec['LOG']);
+  outHash($rec, $out);
+  $out['USERS']=SQLSelect("SELECT * FROM users ORDER BY NAME");
+ }
+/**
+* btdevices delete record
+*
+* @access public
+*/
+ function delete_btdevices($id) {
+  $rec=SQLSelectOne("SELECT * FROM btdevices WHERE ID='$id'");
+  // some action for related tables
+  SQLExec("DELETE FROM btdevices WHERE ID='".$rec['ID']."'");
+ }
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($parent_name="") {
+  parent::install($parent_name);
+ }
+/**
+* Uninstall
+*
+* Module uninstall routine
+*
+* @access public
+*/
+ function uninstall() {
+  SQLExec('DROP TABLE IF EXISTS btdevices');
+  parent::uninstall();
+ }
+/**
+* dbInstall
+*
+* Database installation routine
+*
+* @access private
+*/
+ function dbInstall($data) {
+/*
+btdevices - btdevices
+*/
+  $data = <<<EOD
+ btdevices: ID int(10) unsigned NOT NULL auto_increment
+ btdevices: TITLE varchar(255) NOT NULL DEFAULT ''
+ btdevices: MAC varchar(255) NOT NULL DEFAULT ''
+ btdevices: LOG text NOT NULL DEFAULT ''
+ btdevices: LAST_FOUND datetime
+ btdevices: FIRST_FOUND datetime
+ btdevices: SCRIPT_ID int(10) NOT NULL DEFAULT '0'
+ btdevices: USER_ID int(10) NOT NULL DEFAULT '0'
+EOD;
+  parent::dbInstall($data);
+ }
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgTWFyIDI3LCAyMDA5IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
+?>
---
+++ scripts/cycle_bluetooth.php
@@ -0,0 +1,238 @@
+<?php
+
+/*
+ * @version 0.4 (06.09.2011 bug fixed)
+ */
+
+chdir(dirname(__FILE__) . '/../');
+
+include_once("./config.php");
+include_once("./lib/loader.php");
+include_once("./lib/threads.php");
+
+set_time_limit(0);
+
+// connecting to database
+$db = new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME);
+
+include_once("./load_settings.php");
+include_once(DIR_MODULES . "control_modules/control_modules.class.php");
+
+$ctl = new control_modules();
+
+/*
+if (!Defined('SETTINGS_BLUETOOTH_CYCLE') || SETTINGS_BLUETOOTH_CYCLE == 0)
+   exit;
+*/
+
+$bt_devices = array();
+
+//windows file
+$devices_file = SERVER_ROOT . "/apps/bluetoothview/devices.txt";
+
+//linux command
+$bts_cmd = 'sudo hcitool scan | grep ":"';
+
+$first_run    = 1;
+$skip_counter = 0;
+
+echo "Running bluetooth scanner\n";
+
+while (1)
+{
+   $skip_counter++;
+   if ($skip_counter >= 30)
+   {
+      $skip_counter = 0;
+      $data = '';
+      
+      if (IsWindowsOS())
+      {
+         // windows scanner
+         $isDelete = unlink($devices_file);
+         exec(SERVER_ROOT . '/apps/bluetoothview/bluetoothview.exe /stab ' . $devices_file);
+         
+         if (file_exists($devices_file))
+         {
+            $data = (LoadFile($devices_file));
+            sleep(5);
+         }
+      }
+      else
+      {
+         //linux scanner
+         $str=exec($bts_cmd, $bt_scan_arr);
+         
+         $lines = array();
+         $btScanArrayLength = count($bt_scan_arr);
+         
+         for ($i = 0; $i < $btScanArrayLength; $i++)
+         {
+            if (!$bt_scan_arr[$i]) {
+             continue;
+            }
+            $btstr      = explode("\t", $bt_scan_arr[$i]);
+            $btaddr[$i] = $btstr[1];
+            $btname[$i] = rtrim($btstr[2]);
+            $lines[]    = $i . "\t" . $btname[$i] . "\t" . $btaddr[$i];
+         }
+         
+         $data = implode("\n",$lines);
+      }
+      
+      $last_scan = time();
+
+      if ($data)
+      {
+         $data = str_replace(chr(0), '', $data);
+         $data = str_replace("\r", '', $data);
+         $lines = explode("\n", $data);
+         $total = count($lines);
+         
+         for ($i = 0; $i < $total; $i++)
+         {
+            $fields = explode("\t", $lines[$i]);
+            $title  = trim($fields[1]);
+            $mac    = trim($fields[2]);
+            $user   = array();
+            
+            if ($mac != '')
+            {
+               if (!$bt_devices[$mac])
+               {
+                  // && !$first_run
+                  //new device found
+                  echo date('Y/m/d H:i:s') . ' Device found: ' . $mac . '\n';
+                  
+                  $sqlQuery = "SELECT * 
+                                 FROM btdevices 
+                                WHERE MAC LIKE '" . $mac . "'";
+                  
+                  $rec = SQLSelectOne($sqlQuery);
+                  $previous_found = $rec['LAST_FOUND'];
+                  $rec['LAST_FOUND'] = date('Y/m/d H:i:s');
+                  $rec['LOG'] = 'Device found ' . date('Y/m/d H:i:s') . "\n" . $rec['LOG'];
+                  
+                  if (!$rec['ID'])
+                  {
+                     $rec['FIRST_FOUND'] = $rec['LAST_FOUND'];
+                     $previous_found = $rec['LAST_FOUND'];
+                     $rec['MAC'] = strtolower($mac);
+                     
+                     if ($title != '')
+                        $rec['TITLE'] = 'Устройство: ' . $title;
+                     else
+                        $rec['TITLE'] = 'Новое устройство';
+                     
+                     $new = 1;
+                     
+                     SQLInsert('btdevices', $rec);
+                  }
+                  else
+                  {
+                     $new = 0;
+                     
+                     if ($rec['USER_ID'])
+                     {
+                        $sqlQuery = "SELECT * 
+                                       FROM users 
+                                      WHERE ID = '" . $rec['USER_ID'] . "'";
+                        
+                        $user = SQLSelectOne($sqlQuery);
+                     }
+                     
+                     SQLUpdate('btdevices', $rec);
+                  }
+                  
+                  $objectArray = array('mac'            => $mac,
+                                       'user'           => $user['NAME'],
+                                       'new'            => $new,
+                                       'previous_found' => $previous_found,
+                                       'last_found'     => $rec['FIRST_FOUND']);
+                  
+                  $obj=getObject('BlueDev');
+                  if (is_object($obj)) {
+                   $obj->raiseEvent("Found", $objectArray);
+                  }
+               }
+               else
+               {
+                  $sqlQuery = "SELECT * 
+                                 FROM btdevices 
+                                WHERE MAC = '" . $mac . "'";
+                  
+                  $rec = SQLSelectOne($sqlQuery);
+                  $rec['LAST_FOUND'] = date('Y/m/d H:i:s');
+                  
+                  if ($title != '')
+                  {
+                     $rec['TITLE'] = 'Устройство: ' . $title;
+                  }
+                  
+                  if ($rec['ID'])
+                     SQLUpdate('btdevices', $rec);
+               }
+               
+               $bt_devices[$mac] = $last_scan;
+            }
+         }
+
+         foreach ($bt_devices as $k => $v)
+         {
+            if ($v != $last_scan)
+            {
+               //device removed
+               echo date('Y/m/d H:i:s') . ' Device gone: ' . $k . '\n';
+               
+               $user = array();
+               $sqlQuery = "SELECT * 
+                              FROM btdevices 
+                             WHERE MAC = '" . $k . "'";
+               
+               $rec  = SQLSelectOne($sqlQuery);
+               
+               if ($rec['ID'])
+               {
+                  $rec['LOG'] = 'Device lost ' . date('Y/m/d H:i:s') . '\n' . $rec['LOG'];
+                  SQLUpdate('btdevices', $rec);
+                  
+                  if ($rec['USER_ID'])
+                  {
+                     $sqlQuery = "SELECT * 
+                                    FROM users 
+                                   WHERE ID = '" . $rec['USER_ID'] . "'";
+                     
+                     $user = SQLSelectOne($sqlQuery);
+                  }
+               }
+               
+               $objectArray = array('mac'  => $k,
+                                    'user' => $user['NAME']);
+               
+
+               $obj=getObject('BlueDev');
+               if (is_object($obj)) {
+                   $obj->raiseEvent("Lost", $objectArray);
+               }
+
+
+               unset($bt_devices[$k]);
+            }
+         }
+      }
+   }
+
+   $first_run = 0;
+   
+   if (file_exists('./reboot') || IsSet($_GET['onetime']))
+   {
+      $db->Disconnect();
+      exit;
+   }
+   
+   sleep(1);
+}
+
+// closing database connection
+$db->Disconnect();
+
