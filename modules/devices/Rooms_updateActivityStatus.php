d4c98506bb684e2f23ee12ffc6e2da05e755fb3b
tarasfrompir <tarasfrompir@inbox.ru>
2017-09-13 13:09:27 +0300

    Update Rooms_updateActivityStatus.php

--- modules/devices/Rooms_updateActivityStatus.php
+++ modules/devices/Rooms_updateActivityStatus.php
@@ -12,7 +12,8 @@ for ($i = 0; $i < $total; $i++) {
     }
     $rooms[$i]['active'] = getGlobal($rooms[$i]['TITLE'] . '.SomebodyHere');
     $rooms[$i]['time'] = getGlobal($rooms[$i]['TITLE'] . '.LatestActivity');
-    if (!$rooms[$i]['time']) {
+	$rooms[$i]['user'] = getGlobal($rooms[$i]['TITLE'] . '.UserName');
+	if (!$rooms[$i]['time']) {
         $rooms[$i]['time'] = 0;
     }
     $rooms[$i]['diff'] = time() - $rooms[$i]['time'];
@@ -30,12 +31,13 @@ if (!function_exists('cmpRoomsActivity')) {
 usort($rooms, "cmpRoomsActivity");
 
 if (!$rooms[0]['active']) {
-    $somebodyHomeText = LANG_DEVICES_ROOMS_NOBODYHOME." ".LANG_DEVICES_ROOMS_ACTIVITY." " . date('H:i', $rooms[0]['time']) . " (" . $rooms[0]['room'] . ")";
+    $somebodyHomeText = LANG_DEVICES_ROOMS_NOBODYHOME." ".LANG_DEVICES_ROOMS_ACTIVITY." " . date('H:i m.d.y', $rooms[0]['time']) . " (" . $rooms[0]['room'] . "). " . $rooms[0]['user'];
+	setGlobal($rooms[0]['TITLE']. '.UserName','');
 } else {
     $res_rooms = array();
     for ($i = 0; $i < $total; $i++) {
         if ($rooms[$i]['active']) {
-            $res_rooms[] = $rooms[$i]['room'];
+            $res_rooms[] = $rooms[$i]['room']. '. '. $rooms[$i]['user'];
         }
     }
     $somebodyHomeText = LANG_DEVICES_ROOMS_SOMEBODYHOME." ". LANG_DEVICES_ROOMS_ACTIVITY . " " . implode(", ", $res_rooms);
