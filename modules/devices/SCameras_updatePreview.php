009c1760130c0e7ce09b29934132222f1d805ad3
Sergei Jeihala <jey@tut.by>
2017-10-17 12:14:09 +0300

    Styles update
    * More icons for scenes elemets
    * Simple devices styles updated

--- modules/devices/SCameras_updatePreview.php
+++ modules/devices/SCameras_updatePreview.php
@@ -1,11 +1,22 @@
 <?php
 
+$cameraUsername = $this->getProperty('cameraUsername');
+$cameraPassword = $this->getProperty('cameraPassword');
+
 if ($this->getProperty('snapshotURL')) {
     $html='<img src="'.$this->getProperty('snapshotURL').'" width="250"/>';
 } elseif ($this->getProperty('streamURL_HQ')) {
-    $html=processTitle('<a href="'.$this->getProperty('streamURL_HQ').'" target="_blank">[#module name="thumb" url="'.$this->getProperty('streamURL').'" username="'.$this->getProperty('cameraUsername').'" password="'.$this->getProperty('cameraPassword').'" width="250" live="1"#]',$this);
+    $streamURL=$this->getProperty('streamURL_HQ');
+    if ($cameraUsername || $cameraPassword) {
+        $streamURL=str_replace('://','://'.$cameraUsername.':'.$cameraPassword.'@',$streamURL);
+    }
+    $html=processTitle('<a href="'.$streamURL.'" target="_blank">[#module name="thumb" url="'.$this->getProperty('streamURL').'" username="'.$cameraUsername.'" password="'.$cameraPassword.'" width="250" live="0"#]',$this);
 } else {
-    $html=processTitle('<a href="'.$this->getProperty('streamURL').'" target="_blank">[#module name="thumb" url="'.$this->getProperty('streamURL').'" username="'.$this->getProperty('cameraUsername').'" password="'.$this->getProperty('cameraPassword').'" width="250" live="1"#]',$this);
+    $streamURL=$this->getProperty('streamURL');
+    if ($cameraUsername || $cameraPassword) {
+        $streamURL=str_replace('://','://'.$cameraUsername.':'.$cameraPassword.'@',$streamURL);
+    }
+    $html=processTitle('<a href="'.$this->getProperty('$streamURL').'" target="_blank">[#module name="thumb" url="'.$this->getProperty('streamURL').'" username="'.$cameraUsername.'" password="'.$cameraPassword.'" width="250" live="0"#]',$this);
 }
 
 $this->setProperty('previewHTML',$html);
