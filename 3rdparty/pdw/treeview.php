a7365052deb16f66edcd2d8a0ad71f62d660416f
LDV <palacex@gmail.com>
2018-05-12 03:34:43 +0300

    Fix: missing pdw/config.php
    Change htmlMimeMile constructor and few phpDoc description.

--- 3rdparty/pdw/treeview.php
+++ 3rdparty/pdw/treeview.php
@@ -3,16 +3,18 @@
 require_once('functions.php');
 
 // Rootname is name of uploadfolder
-$rootname = array_pop((explode("/", trim($uploadpath,"/"))));
+$uploadPathArr = explode("/", trim($uploadpath,"/"));
+$rootname = array_pop($uploadPathArr);
 
 // Get folders from uploadpath and create a list
 $dirs = getDirTree(STARTINGPATH, false);
 			
 //Print treeview to screen
-echo '<ul class="treeview">
-            <li class="selected"><a class="root" href="'.$uploadpath.'">'.$rootname."</a>\n";
-echo 		renderTree($dirs, $uploadpath);
-echo "            </li>
-       </ul>\n";
+echo '<ul class="treeview">';
+echo '   <li class="selected">';
+echo '      <a class="root" href="' . $uploadpath . '">' . $rootname . '</a>';
+echo 		   renderTree($dirs, $uploadpath);
+echo '   </li>';
+echo '</ul>';
 
 ?>
