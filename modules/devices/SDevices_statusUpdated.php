3514e672f3cdddea34aa664c2ead125c07f5b2b8
Sergei Jeihala <sergejey@gmail.com>
2018-06-12 20:26:34 +0300

    Simple Devices: motion fix
    Simple Devices: motion fix

--- modules/devices/SDevices_statusUpdated.php
+++ modules/devices/SDevices_statusUpdated.php
@@ -1,52 +1,48 @@
 <?php
 
- $ot=$this->object_title;
- $linked_room=$this->getProperty('linkedRoom');
+$ot = $this->object_title;
+$linked_room = $this->getProperty('linkedRoom');
 
- $tm=time();
- $this->setProperty('updated', $tm);
- $this->callMethod('setUpdatedText');
- $this->setProperty('alive', 1);
+$tm = time();
+$this->setProperty('updated', $tm);
+$this->callMethod('setUpdatedText');
+$this->setProperty('alive', 1);
 
 
- $alive_timeout=(int)$this->getProperty('aliveTimeout')*60*60;
- if (!$alive_timeout) {
-  $alive_timeout=2*24*60*60; // 2 days alive timeout by default
- }
+$alive_timeout = (int)$this->getProperty('aliveTimeout') * 60 * 60;
+if (!$alive_timeout) {
+    $alive_timeout = 2 * 24 * 60 * 60; // 2 days alive timeout by default
+}
 
- setTimeout($ot.'_alive_timer', 'setGlobal("'.$ot.'.alive", 0);', $alive_timeout);
+setTimeout($ot . '_alive_timer', 'setGlobal("' . $ot . '.alive", 0);', $alive_timeout);
 
 $need_call_logic_action = 1;
 
-if ($this->class_title == 'SMotions' && $params['NEW_VALUE'] && !timeOutExists($ot.'_motion_timer_status')) {
-    $this->callMethodSafe('motionDetected',array('statusUpdated'=>1));
-} elseif ($this->class_title == 'SButtons' && $params['NEW_VALUE'] && !timeOutExists($ot.'_pressed_status')) {
-    $this->callMethodSafe('pressed',array('statusUpdated'=>1));
-}
-
-if ($this->class_title == 'SMotions') {
- $need_call_logic_action = 0;
+if ($this->class_title == 'SMotions' && $params['NEW_VALUE'] && !timeOutExists($ot . '_motion_timer_status')) {
+    $this->callMethodSafe('motionDetected', array('statusUpdated' => 1));
+} elseif ($this->class_title == 'SButtons' && $params['NEW_VALUE'] && !timeOutExists($ot . '_pressed_status')) {
+    $this->callMethodSafe('pressed', array('statusUpdated' => 1));
 }
 
 if ($params['NEW_VALUE'] && $linked_room && $this->getProperty('isActivity')) {
- if (getGlobal('NobodyHomeMode.active')) {
-  callMethodSafe('NobodyHomeMode.deactivate', array('sensor'=>$ot, 'room'=>$linked_room));
- }
- ClearTimeOut("nobodyHome");
- SetTimeOut("nobodyHome","callMethodSafe('NobodyHomeMode.activate');", 1*60*60);
- if ($linked_room) {
-  callMethodSafe($linked_room.'.onActivity', array('sensor'=>$ot));
- }
+    if (getGlobal('NobodyHomeMode.active')) {
+        callMethodSafe('NobodyHomeMode.deactivate', array('sensor' => $ot, 'room' => $linked_room));
+    }
+    ClearTimeOut("nobodyHome");
+    SetTimeOut("nobodyHome", "callMethodSafe('NobodyHomeMode.activate');", 1 * 60 * 60);
+    if ($linked_room) {
+        callMethodSafe($linked_room . '.onActivity', array('sensor' => $ot));
+    }
 }
 
 if ($need_call_logic_action) {
- /*
- if ($this->class_title == 'SRelays') {
-  DebMes($this->object_title.' '.$params['NEW_VALUE'],'logic_test');
- }
- */
- $this->callMethod('logicAction');
- include_once(DIR_MODULES.'devices/devices.class.php');
- $dv=new devices();
- $dv->checkLinkedDevicesAction($this->object_title, $value);
+    /*
+    if ($this->class_title == 'SRelays') {
+     DebMes($this->object_title.' '.$params['NEW_VALUE'],'logic_test');
+    }
+    */
+    $this->callMethod('logicAction');
+    include_once(DIR_MODULES . 'devices/devices.class.php');
+    $dv = new devices();
+    $dv->checkLinkedDevicesAction($this->object_title, $value);
 }
