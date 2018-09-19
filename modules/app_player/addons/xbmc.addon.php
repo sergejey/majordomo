02c3df5fafd1d4d2c5c38769c393fdab66802ed5
tarasfrompir <tarasfrompir@inbox.ru>
2018-09-19 21:49:19 +0300

    Create xbmc.addon.php

---
+++ modules/app_player/addons/xbmc.addon.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Addon XBMC for app_player
+*/
+class xbmc extends app_player_addon {
+	// Constructor
+	function __construct($terminal) {
+		$this->title = 'XBMC (Kodi)';
+		$this->description = 'Бесплатный кроссплатформенный медиаплеер и программное обеспечение для организации HTPC с открытым исходным кодом.';
+		
+		$this->terminal = $terminal;
+		$this->reset_properties();
+	}
+	/*
+		В процессе портирования
+	*/
+}
