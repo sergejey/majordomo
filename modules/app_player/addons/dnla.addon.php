109f31733653f16678ec187ae58e787f91b3a6b3
tarasfrompir <tarasfrompir@inbox.ru>
2018-11-07 21:42:33 +0200

    Update dnla.addon.php

--- modules/app_player/addons/dnla.addon.php
+++ modules/app_player/addons/dnla.addon.php
@@ -71,27 +71,34 @@ class dnla extends app_player_addon {
         $volume = $doc->getElementsByTagName('CurrentVolume')->item(0)->nodeValue;
         // Для получения состояния плеера
         $remote = new MediaRenderer($current_dev);
+        $response = $remote->getState();
+        $doc->loadXML($response);
+        $state = $doc->getElementsByTagName('CurrentTransportState')->item(0)->nodeValue;
+        $current_speed = $doc->getElementsByTagName('CurrentSpeed')->item(0)->nodeValue;
+        //Debmes ('current_speed '.$current_speed);
         $response = $remote->getPosition();
         $doc->loadXML($response);
         $track_id = $doc->getElementsByTagName('Track')->item(0)->nodeValue;
         $length = $this->parse_to_second($doc->getElementsByTagName('TrackDuration')->item(0)->nodeValue);
         $time = $this->parse_to_second($doc->getElementsByTagName('RelTime')->item(0)->nodeValue);
         $curren_url = $doc->getElementsByTagName('TrackURI')->item(0)->nodeValue;
-	// Results
-	$this->reset_properties();
-	$this->success = TRUE;
-	$this->message = 'OK';
-	$this->data = array(
-		'track_id'		=> (int)$track_id, //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
-		'length'		=> (int)$length, //Track length in seconds. Integer. If unknown = 0. 
-		'time'			=> (int)$time, //Current playback progress (in seconds). If unknown = 0. 
-		'state'			=> (string)$state, //Playback status. String: stopped/playing/paused/unknown 
-		'volume'		=> (int)$volume, // Volume level in percent. Integer. Some players may have values greater than 100.
-		'random'		=> (boolean)$random, // Random mode. Boolean. 
-		'loop'			=> (boolean)$loop, // Loop mode. Boolean.
-		'repeat'		=> (boolean)$repeat, //Repeat mode. Boolean.
-			);
-	return $this->success;	
+        // Results
+        $this->reset_properties();
+        $this->success = TRUE;
+        $this->message = 'OK';
+        $this->data = array(
+            'track_id'        => (int)$track_id, //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
+            'length'          => (int)$length, //Track length in seconds. Integer. If unknown = 0. 
+            'time'            => (int)$time, //Current playback progress (in seconds). If unknown = 0. 
+            'state'           => (string)$state, //Playback status. String: stopped/playing/paused/unknown 
+            'volume'          => (int)$volume, // Volume level in percent. Integer. Some players may have values greater than 100.
+            'random'          => (boolean)$random, // Random mode. Boolean. 
+            'loop'            => (boolean)$loop, // Loop mode. Boolean.
+            'repeat'          => (boolean)$repeat, //Repeat mode. Boolean.
+            'speed'           => (float)$current_speed, //Current speed for playing media. float.
+            'link'            => (string)$curren_url, //Current link for media in device. String.
+            );
+        return $this->success;    
     }
 
     // Play
@@ -101,13 +108,16 @@ class dnla extends app_player_addon {
         $current_dev = str_ireplace("Location:", "", $current_dev);
         $remote = new MediaRenderer($current_dev);
         // для радио 101 ру
-	if( stripos( $input, '?userid=0&setst') ) {
+        if( stripos( $input, '?userid=0&setst') ) {
             $input = stristr($input, '&setst', True);
             }
-        DebMes('Ссылка подана на плеер - '.$input);
-	$answer = $remote->play($input);
-        DebMes($answer);
-        if($answer) {
+        DebMes('Ссылка '.$input.' подана на терминал - '.$this->terminal['NAME']);
+        $response = $remote->play($input);
+        // создаем хмл документ
+        $doc = new \DOMDocument();
+        $doc->loadXML($response);
+        //DebMes($response);
+        if($doc->getElementsByTagName('PlayResponse')) {
             $this->success = TRUE;
             $this->message = 'Play files';
          } else {
@@ -123,8 +133,12 @@ class dnla extends app_player_addon {
         $current_dev = ($this->terminal['PLAYER_CONTROL_ADDRESS']);
         $current_dev = str_ireplace("Location:", "", $current_dev);
         $remote = new MediaRenderer($current_dev);
-        $answer = $remote->stop();
-        if($answer) {
+        $response = $remote->stop();
+        // создаем хмл документ
+        $doc = new \DOMDocument();
+        $doc->loadXML($response);
+        //DebMes($response);
+        if($doc->getElementsByTagName('StopResponse ')) {
             $this->success = TRUE;
             $this->message = 'Stop play';
          } else {
@@ -137,11 +151,15 @@ class dnla extends app_player_addon {
     // Pause
     function pause() {
         $this->reset_properties();
-       $current_dev = ($this->terminal['PLAYER_CONTROL_ADDRESS']);
+        $current_dev = ($this->terminal['PLAYER_CONTROL_ADDRESS']);
         $current_dev = str_ireplace("Location:", "", $current_dev);
         $remote = new MediaRenderer($current_dev);
-        $answer = $remote->pause();
-        if($answer) {
+        $response = $remote->pause();
+        // создаем хмл документ
+        $doc = new \DOMDocument();
+        $doc->loadXML($response);
+        //DebMes($response);
+        if($doc->getElementsByTagName('PauseResponse ')) {
             $this->success = TRUE;
             $this->message = 'Pause enabled';
          } else {
@@ -157,8 +175,12 @@ class dnla extends app_player_addon {
         $current_dev = ($this->terminal['PLAYER_CONTROL_ADDRESS']);
         $current_dev = str_ireplace("Location:", "", $current_dev);
         $remote = new MediaRenderer($current_dev);
-        $answer = $remote->next();
-        if($answer) {
+        $response = $remote->next();
+        // создаем хмл документ
+        $doc = new \DOMDocument();
+        $doc->loadXML($response);
+        //DebMes($response);
+        if($doc->getElementsByTagName('NextResponse')) {
             $this->success = TRUE;
             $this->message = 'Next file changed';
          } else {
@@ -174,8 +196,12 @@ class dnla extends app_player_addon {
         $current_dev = ($this->terminal['PLAYER_CONTROL_ADDRESS']);
         $current_dev = str_ireplace("Location:", "", $current_dev);
         $remote = new MediaRenderer($current_dev);
-        $answer = $remote->previous();
-        if($answer) {
+        $response = $remote->previous();
+        // создаем хмл документ
+        $doc = new \DOMDocument();
+        $doc->loadXML($response);
+        //DebMes($response);
+        if($doc->getElementsByTagName('PreviousResponse')) {
             $this->success = TRUE;
             $this->message = 'Previous file changed';
          } else {
@@ -202,7 +228,7 @@ class dnla extends app_player_addon {
             $this->success = TRUE;
             $this->message = 'Volume changed';
          } else {
-	    DebMes('Громкость на терминале - '.$this->terminal['NAME'].' НЕ ИЗМЕНЕНА ОШИБКА!');
+            DebMes('Громкость на терминале - '.$this->terminal['NAME'].' НЕ ИЗМЕНЕНА ОШИБКА!');
             $this->success = FALSE;
             $this->message = 'Command execution error!';
             }
@@ -225,7 +251,7 @@ class dnla extends app_player_addon {
         $request .= "\r\n";
         
         @socket_sendto($socket, $request, strlen($request), 0, '255.255.255.255', 1900);
-	@socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1900);
+        @socket_sendto($socket, $request, strlen($request), 0, '239.255.255.250', 1900);
 
         // send the data from socket
         socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=>'1', 'usec'=>'128'));
@@ -248,15 +274,15 @@ class dnla extends app_player_addon {
             }
         } while(!is_null($buf));
         socket_close($socket);
-          $response = str_ireplace("Location:", "", $response);
+        $response = str_ireplace("Location:", "", $response);
         return $response;
     } 
 
     // функция преобразования в секунды времени
-     function parse_to_second($time) {
-	$parsed = date_parse($time);
-	$seconds = $parsed['hour'] * 3600 + $parsed['minute'] * 60 + $parsed['second'];
+    function parse_to_second($time) {
+        $parsed = date_parse($time);
+        $seconds = $parsed['hour'] * 3600 + $parsed['minute'] * 60 + $parsed['second'];
         return $seconds;
-        }
+    }
 }
 ?>
