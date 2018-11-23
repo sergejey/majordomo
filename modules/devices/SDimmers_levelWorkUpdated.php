dbb6921846fcb5e17359c789ffbfd3d9f46ae605
Sergei Jeihala <sergejey@gmail.com>
2018-11-23 16:48:46 +0300

    Update SDimmers_levelWorkUpdated.php
    fix

--- modules/devices/SDimmers_levelWorkUpdated.php
+++ modules/devices/SDimmers_levelWorkUpdated.php
@@ -3,7 +3,7 @@
 $level=$this->getProperty('level');
 $minWork=$this->getProperty('minWork');
 $maxWork=$this->getProperty('maxWork');
-$levelWork=$this->setProperty('levelWork');
+$levelWork=$this->getProperty('levelWork'); //
 if ($minWork!=$maxWork) {
     $new_level=round((($levelWork-$minWork)/($maxWork-$minWork))*100);
     if ($new_level!=$level) {
