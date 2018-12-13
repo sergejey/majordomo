aae6d7f0183f5bb85802feacff14e406c67de5a1
devoff <4802297@gmail.com>
2018-12-13 18:22:39 +0300

    Update Rooms_onActivity.php
    ($this->getProperty('SomebodyHere')>0) 
    нужно перенести в самый вверх, так как при таком исполнение запускается весь нижний блок, а должен только при изменение SomebodyHere
    
    так же прошивка  wifi iot присылет не булен, а не 0 и 1, поэтому не работает через ! 
    
    Добавил так же свойство LatestActivityDate',date('d.m.Y')); что бы записывалась и последнее дата движения, очень нужно например на даче !

--- modules/devices/Rooms_onActivity.php
+++ modules/devices/Rooms_onActivity.php
@@ -2,26 +2,31 @@
 
 if (defined('DISABLE_SIMPLE_DEVICES') && DISABLE_SIMPLE_DEVICES==1) return;
 
-$ot=$this->object_title;
+if ($this->getProperty('SomebodyHere')>0) {
 
-$latestActivity=$this->getProperty('LatestActivity');
-$this->setProperty('LatestActivity',time());
-$this->setProperty('LatestActivityTime',date('H:i'));
+  $ot=$this->object_title;
 
-if (!$this->getProperty('SomebodyHere')) {
+  $latestActivity=$this->getProperty('LatestActivity');
+  $this->setProperty('LatestActivity',time());
+  $this->setProperty('LatestActivityTime',date('H:i'));
+  $this->setProperty('LatestActivityDate',date('d.m.Y'));
+
+
     $this->setProperty('SomebodyHere',1);
-}
 
-if ($this->getProperty("IdleDelay")) {
-    $activity_timeout=(int)$this->getProperty("IdleDelay");
-} else {
-    $activity_timeout=10*60;
-}
-clearTimeOut($ot."_activity_timeout");
-setTimeOut($ot."_activity_timeout","callMethodSafe('".$ot.".onIdle');",$activity_timeout);
 
-$this->callMethodSafe("updateActivityStatus");
+  if ($this->getProperty("IdleDelay")) {
+      $activity_timeout=(int)$this->getProperty("IdleDelay");
+  } else {
+      $activity_timeout=10*60;
+  }
+  clearTimeOut($ot."_activity_timeout");
+  setTimeOut($ot."_activity_timeout","callMethodSafe('".$ot.".onIdle');",$activity_timeout);
 
-if (getGlobal('NobodyHomeMode.active')) {
-    callMethodSafe('NobodyHomeMode.deactivate',array('sensor'=>$params['sensor'],'room'=>$ot));
+  $this->callMethodSafe("updateActivityStatus");
+
+  if (getGlobal('NobodyHomeMode.active')) {
+      callMethodSafe('NobodyHomeMode.deactivate',array('sensor'=>$params['sensor'],'room'=>$ot));
+  }
+
 }
