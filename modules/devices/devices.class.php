e37af0455846f167d2ef554e2841854fd0ade166
Andrey Isupov <eraser1981@gmail.com>
2019-03-26 09:20:42 +0300

    Simple Devices: fix run sheduler task

--- modules/devices/devices.class.php
+++ modules/devices/devices.class.php
@@ -463,6 +463,10 @@ class devices extends module
                 if ($diff < 0 || $diff >= 10 * 60) {
                     continue;
                 }
+                $tmlr = strtotime($rec['LATEST_RUN']);
+                $diff_run = time() - $tmlr;
+                if ($diff_run <= 20*60)
+                    continue;
                 $linked_object = $rec['LINKED_OBJECT'];
                 unset($rec['LINKED_OBJECT']);
                 $rec['LATEST_RUN'] = date('Y-m-d H:i:s');
