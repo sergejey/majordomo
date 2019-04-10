67750310676f208bf040481b3702a8c5ec45edf5
tarasfrompir <tarasfrompir@inbox.ru>
2019-04-10 21:41:06 +0300

    Update chromecast.addon.php

--- modules/app_player/addons/chromecast.addon.php
+++ modules/app_player/addons/chromecast.addon.php
@@ -13,46 +13,74 @@ class chromecast extends app_player_addon {
 		
 		$this->terminal = $terminal;
 		$this->reset_properties();
-		
 		// Network
 		$this->terminal['PLAYER_PORT'] = (empty($this->terminal['PLAYER_PORT'])?8009:$this->terminal['PLAYER_PORT']);
 		
 		// Chromecast
 		include_once(DIR_MODULES.'app_player/libs/castv2/Chromecast.php');
 	}
-
-	/*
 	// Get player status
 	function status() {
 		$this->reset_properties();
-		try {
-			$cc = new GChromecast($this->terminal['HOST'], this->terminal['PLAYER_PORT']);
-			$cc->requestId = time();
-			
-			var_dump(
-				$cc->DMP->getStatus() // TODO
-			);
-			
-			//$this->success = TRUE;
-			//$this->message = 'OK';
-			//$this->data = array(
-			//	'track_id'		=> -1,
-			//	'length'		=> 0,
-			//	'time'			=> 0,
-			//	'state'			=> 'unknown',
-			//	'volume'		=> 100,
-			//	'random'		=> FALSE,
-			//	'loop'			=> FALSE,
-			//	'repeat'		=> FALSE,
-			//);
-			
-		} catch (Exception $e) {
-			$this->success = FALSE;
-			$this->message = $e->getMessage();
+		// Defaults
+        $track_id      = -1;
+        $length        = 0;
+        $time          = 0;
+        $state         = 'unknown';
+        $volume        = 0;
+        $random        = FALSE;
+        $loop          = FALSE;
+        $repeat        = FALSE;
+		
+		$cc = new GChromecast($this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+		$cc->requestId = time();
+		$result = $cc->DMP->getStatus();
+
+        if ($result) {
+			$this->reset_properties();
+            $this->success = TRUE;
+            $this->message = 'OK';
+            $this->data = array(
+            'track_id'        => (int)$result['status'][0]['media']['tracks'][0]['trackId'], //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
+            'length'          => (int)$result['status'][0]['media']['duration'], //Track length in seconds. Integer. If unknown = 0. 
+            'time'            => (int)$result['status'][0]['currentTime'], //Current playback progress (in seconds). If unknown = 0. 
+            'state'           => (string)strtolower($result['status'][0]['playerState']), //Playback status. String: stopped/playing/paused/unknown 
+            'volume'          => (int)$volume, // Volume level in percent. Integer. Some players may have values greater than 100.
+            'random'          => (boolean)$random, // Random mode. Boolean. 
+            'loop'            => (boolean)$loop, // Loop mode. Boolean.
+            'repeat'          => (boolean)$repeat, //Repeat mode. Boolean.
+            );
 		}
-		return $this->success;
+        return $this->success;   
 	}
-	*/
+
+	    // Playlist: Get
+    function pl_get() {
+        $this->success = FALSE;
+        $this->message = 'Command execution error!';    
+        $track_id      = -1;
+        $name          = 'unknow';
+        $curren_url    = '';
+
+		$cc = new GChromecast($this->terminal['HOST'], $this->terminal['PLAYER_PORT']);
+		$cc->requestId = time();
+		$result = $cc->DMP->getStatus();
+		// дает ответ надо его распарсить
+		DebMes ($result['status'][0]['media']['duration']);
+		
+        if ($result) {
+            // Results
+            $this->reset_properties();
+            $this->success = TRUE;
+            $this->message = 'OK';
+            $this->data = array(
+                'id'        => (int)$result['status'][0]['media']['tracks'][0]['trackId'], //ID of currently playing track (in playlist). Integer. If unknown (playback stopped or playlist is empty) = -1.
+                'name'      => (string)$name, //Current speed for playing media. float.
+                'file'      => (string)$result['status'][0]['media']['contentId'], //Current link for media in device. String.
+                );
+        }
+        return $this->success;    
+    }
 	
 	// Play
 	function play($input) {
