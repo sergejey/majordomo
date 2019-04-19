508e5fee42119a0c5a971059a7e656425f050525
SergeJey <sergejey@gmail.com>
2019-04-19 11:04:58 +0300

    API update and cleanup
    * API new option to request multiple properties
    * Simple Devices: dimmers linked action update
    * Some files removed (themes)

--- modules/devices/SDimmers_statusUpdated.php
+++ modules/devices/SDimmers_statusUpdated.php
@@ -5,4 +5,9 @@ $level=$this->getProperty('level');
 $levelSaved=$this->getProperty('levelSaved');
 if ($status>0 && !$level && $levelSaved) {
     $this->setProperty('level',$levelSaved);
+} else {
+    $this->callMethod('logicAction');
+    include_once(DIR_MODULES . 'devices/devices.class.php');
+    $dv = new devices();
+    $dv->checkLinkedDevicesAction($this->object_title, $level);
 }
