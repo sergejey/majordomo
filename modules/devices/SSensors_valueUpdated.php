1f8a2f28c0a69cb8146c1ff3b8c8f15ec7a1b8a4
Alexandr-logrus <nechaev.alex@gmail.com>
2019-05-08 23:55:27 +0500

    fix

--- modules/devices/SSensors_valueUpdated.php
+++ modules/devices/SSensors_valueUpdated.php
@@ -1,59 +1,58 @@
 <?php
 
- //$this->callMethod('logicAction');
-
- $ot=$this->object_title;
- $description = $this->description;
- if (!$description) {
+$ot = $this->object_title;
+$description = $this->description;
+if (!$description) {
   $description = $ot;
- }
- $linked_room=$this->getProperty('linkedRoom');
+}
 
- $value=(float)$this->getProperty('value');
- $minValue=(float)$this->getProperty('minValue');
- $maxValue=(float)$this->getProperty('maxValue');
- $is_normal=(int)$this->getProperty('normalValue');
- $directionTimeout=(int)$this->getProperty('directionTimeout');
- if (!$directionTimeout) {
-  $directionTimeout=1*60*60;
- }
+$directionTimeout = (int)$this->getProperty('directionTimeout');
+if (!$directionTimeout) {
+  $directionTimeout = 1*60*60;
+}
 
- if ($maxValue==0 && $minValue==0 && !$is_normal) {
+$value = (float)$params['NEW_VALUE'];
+$minValue = (float)$this->getProperty('minValue');
+$maxValue = (float)$this->getProperty('maxValue');
+$is_normal = (int)$this->getProperty('normalValue');
+if ($maxValue == 0 && $minValue == 0 && !$is_normal) {
   $this->setProperty('normalValue', 1);
- } elseif (($value>$maxValue || $value<$minValue) && $is_normal) {
+} elseif (($value > $maxValue || $value < $minValue) && $is_normal) {
   $this->setProperty('normalValue', 0);
   if ($this->getProperty('notify')) {
-   //out of range notify
-   say(LANG_DEVICES_NOTIFY_OUTOFRANGE. ' ('.$description.' '.$value.')', 2);
+    //out of range notify
+    say(LANG_DEVICES_NOTIFY_OUTOFRANGE . ' (' . $description . ' ' . $value . ')', 2);
   }
- } elseif (($value<=$maxValue && $value>=$minValue) && !$is_normal) {
+} elseif (($value <= $maxValue && $value >= $minValue) && !$is_normal) {
   $this->setProperty('normalValue', 1);
   if ($this->getProperty('notify')) {
-   //back to normal notify
-   say(LANG_DEVICES_NOTIFY_BACKTONORMAL. ' ('.$description.' '.$value.')', 2);
+    //back to normal notify
+    say(LANG_DEVICES_NOTIFY_BACKTONORMAL . ' (' . $description . ' ' . $value . ')', 2);
   }
- }
+}
 
+$data1 = getHistoryValue($this->object_title . '.value', time() - $directionTimeout);
+$direction = 0;
+if ($data1 > $value) {
+  $direction = -1;
+} elseif ($data1 < $value) {
+  $direction = 1;
+}
+$currentDirection = $this->getProperty('direction');
+if ($currentDirection != $direction) {
+  $this->setProperty('direction', $direction);
+}
 
- $data1 = getHistoryValue($this->object_title.".value", time()-$directionTimeout);
- $direction = 0;
- if ($data1>$value) {
-  $direction=-1;
- } elseif ($data1<$value) {
-  $direction=1;
- }
- $currentDirection = $this->getProperty('direction');
- if ($currentDirection != $direction) {
-  $this->setProperty('direction',$direction);
- }
-
- if ($linked_room && $this->getProperty('mainSensor')) {
-  if ($this->class_title=='STempSensors') {
-   sg($linked_room.'.temperature',$value);
-  } elseif ($this->class_title=='SHumSensors') {
-   sg($linked_room.'.humidity',$value);
+$linked_room = $this->getProperty('linkedRoom');
+if ($linked_room && $this->getProperty('mainSensor')) {
+  if ($this->class_title == 'STempSensors') {
+    setGlobal($linked_room . '.temperature', $value);
+  } elseif ($this->class_title == 'SHumSensors') {
+    setGlobal($linked_room . '.humidity', $value);
+  } elseif ($this->class_title == 'SLightSensors') {
+    setGlobal($linked_room . '.light', $value);
   }
- }
+}
 
 $this->callMethod('statusUpdated');
 /*
