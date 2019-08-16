ca4ae35dfccea9fbdaa3265ae039c33e36f8d5d8
tarasfrompir <tarasfrompir@inbox.ru>
2019-08-16 10:04:35 +0300

    add function get_remote_filesize

--- lib/common.class.php
+++ lib/common.class.php
@@ -1328,3 +1328,16 @@ function logAction($action_type,$details='') {
     SQLInsert('actions_log',$rec);
 
 }
+
+function get_remote_filesize($url)
+{
+    $head = array_change_key_case(get_headers($url, 1));
+    // content-length of download (in bytes), read from Content-Length: field
+    $clen = isset($head['content-length']) ? $head['content-length'] : 0;
+
+    // cannot retrieve file size, return "-1"
+    if (!$clen) {
+        return '0';
+    }
+    return $clen; // return size in bytes
+}
