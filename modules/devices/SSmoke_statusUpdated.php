dca47ced1ab95f35110057fc76318d9bdb934a2e
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2019-08-24 20:47:17 +0500

    Update SSmoke_statusUpdated.php
    add alarm for smoke sensor

--- modules/devices/SSmoke_statusUpdated.php
+++ modules/devices/SSmoke_statusUpdated.php
@@ -8,6 +8,24 @@ if ($this->getProperty('alive') == 0) {
  $this->setProperty('alive', 1);
 }
 
+
+$sql=' select *, locations.TITLE TITLELOC from objects, locations, devices where objects.LOCATION_ID=locations.ID and objects.id='.$this->id.' and devices.LINKED_OBJECT=objects.TITLE ';
+$loc=SQLSelectOne($sql)['TITLELOC'];
+//say ($this->getProperty('notify_status'), 2);
+if ($this->getProperty('notify_status')) {
+    if (isset($params['NEW_VALUE'])) {
+        if ($params['NEW_VALUE']==1) 
+{
+saySafe(LANG_ALERT.' '.mb_strtolower(LANG_SENSOR_ALARM).' '.mb_strtolower(LANG_DEVICES_SMOKE_SENSOR).' '.LANG_IN.' '.mb_strtolower($loc) , 2); 
+//say('111', 2); 
+}
+else 
+{saySafe(LANG_DEVICES_SMOKE_SENSOR.' '.LANG_IN.' '.mb_strtolower($loc).' ' .LANG_SENSOR_NORMAL, 2); 
+}
+}
+}
+
+
 $alive_timeout = (int)$this->getProperty('aliveTimeout')*60*60;
 if (!$alive_timeout) {
     $alive_timeout = 2*24*60*60; // 2 days alive timeout by default
