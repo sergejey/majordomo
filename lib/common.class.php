26822d3c6b09f68ba6b8949ea6e9db1d10adc76d
Alexandr-logrus <nechaev.alex@gmail.com>
2020-03-24 23:07:11 +0500

    Update common.class.php

--- lib/common.class.php
+++ lib/common.class.php
@@ -189,8 +189,8 @@ function say($ph, $level = 0, $member_id = 0, $source = '')
     }
 
 
-    if ($level >= (int)getGlobal('minMsgLevel') && !$ignoreVoice && !$member_id) {
-        if (!defined('SETTINGS_SPEAK_SIGNAL') || SETTINGS_SPEAK_SIGNAL == '1') {
+    if (!defined('SETTINGS_SPEAK_SIGNAL') || SETTINGS_SPEAK_SIGNAL == '1') {
+        if ($level >= (int)getGlobal('minMsgLevel') && !$member_id) { // && !$ignoreVoice
             $passed = time() - (int)getGlobal('lastSayTime');
             if ($passed > 20) {
                 playSound('dingdong', 1, $level);
