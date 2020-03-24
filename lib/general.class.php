fb2bad34d89ad306b4343717410c4122a1867875
Alexandr-logrus <nechaev.alex@gmail.com>
2020-03-24 23:18:39 +0500

    Update general.class.php

--- lib/general.class.php
+++ lib/general.class.php
@@ -9,52 +9,32 @@
  * @version 1.3
  */
 
-if (defined('HOME_NETWORK') && HOME_NETWORK != '' && !isset($argv[0])
-    && (!(preg_match('/\/gps\.php/is', $_SERVER['REQUEST_URI'])
-       || preg_match('/\/trackme\.php/is', $_SERVER['REQUEST_URI'])
-       || preg_match('/\/btraced\.php/is', $_SERVER['REQUEST_URI']))
-       || $_REQUEST['op'] != '')
-    && !preg_match('/\/rss\.php/is', $_SERVER['REQUEST_URI'])
-    && 1)
-{
-   $p = preg_quote(HOME_NETWORK);
-   $p = str_replace('\*', '\d+?', $p);
-   $p = str_replace(',', ' ', $p);
-   $p = str_replace('  ', ' ', $p);
-   $p = str_replace(' ', '|', $p);
-
-   $remoteAddr = getenv('HTTP_X_FORWARDED_FOR') ? getenv('HTTP_X_FORWARDED_FOR') : $_SERVER["REMOTE_ADDR"];
-
-   if (!preg_match('/' . $p . '/is', $remoteAddr) && $remoteAddr != '127.0.0.1' && trim($remoteAddr)!= '::1')
-   {
-      // password required
-      //echo "password required for ".$remoteAddr;exit;
-      //DebMes("checking access for ".$remoteAddr);
-
-      if (!isset($_SERVER['PHP_AUTH_USER']))
-      {
-         header("WWW-Authenticate: Basic realm=\"" . PROJECT_TITLE . "\"");
-         header("HTTP/1.0 401 Unauthorized");
-         echo "Authorization required\n";
-         exit;
-      }
-      else
-      {
-         if ($_SERVER['PHP_AUTH_USER'] != EXT_ACCESS_USERNAME || $_SERVER['PHP_AUTH_PW'] != EXT_ACCESS_PASSWORD)
-         {
+if (defined('HOME_NETWORK') && HOME_NETWORK != '' && !isset($argv[0]) && (!(preg_match('/\/gps\.php/is', $_SERVER['REQUEST_URI']) || preg_match('/\/trackme\.php/is', $_SERVER['REQUEST_URI']) || preg_match('/\/btraced\.php/is', $_SERVER['REQUEST_URI']) || preg_match('/\/rss\.php/is', $_SERVER['REQUEST_URI'])) || $_REQUEST['op'] != '')) {
+    $p = preg_quote(HOME_NETWORK);
+    $p = str_replace('\*', '\d+?', $p);
+    $p = str_replace(',', ' ', $p);
+    $p = str_replace('  ', ' ', $p);
+    $p = str_replace(' ', '|', $p);
+    
+    $remoteAddr = getenv('HTTP_X_FORWARDED_FOR') ? getenv('HTTP_X_FORWARDED_FOR') : $_SERVER["REMOTE_ADDR"];
+    
+    if (!preg_match('/' . $p . '/is', $remoteAddr) && $remoteAddr != '127.0.0.1' && trim($remoteAddr) != '::1') {
+        if (defined('EXT_ACCESS_USERNAME') && defined('EXT_ACCESS_PASSWORD') && $_SERVER['PHP_AUTH_USER'] == EXT_ACCESS_USERNAME && $_SERVER['PHP_AUTH_PW'] == EXT_ACCESS_PASSWORD) {
+            $data = $_SERVER['REMOTE_ADDR'] . " " . date("[d/m/Y:H:i:s]") . " Username and/or password valid. Login: " . $_SERVER['PHP_AUTH_USER'] . " Password: " . $_SERVER['PHP_AUTH_PW'] . "\n";
+            DebMes($data, 'auth');
+        } elseif (!defined('EXT_ACCESS_USERNAME') && !defined('EXT_ACCESS_PASSWORD')) {
+            $data = $_SERVER['REMOTE_ADDR'] . " " . date("[d/m/Y:H:i:s]") . " Username and/or password dont defined and dont needed" . "\n";
+            DebMes($data, 'auth');
+        } else {
             // header("Location:$PHP_SELF\n\n");
             header("WWW-Authenticate: Basic realm=\"" . PROJECT_TITLE . "\"");
             header("HTTP/1.0 401 Unauthorized");
             echo "Authorization required\n";
-            
-            $data = $_SERVER['REMOTE_ADDR'] . " " . date("[d/m/Y:H:i:s]") . " Username and/or password invalid. Login: " .
-                $_SERVER['PHP_AUTH_USER'] . " Password: " . $_SERVER['PHP_AUTH_PW'] . "\n";
-            DebMes($data,'auth');
-            
+            $data = $_SERVER['REMOTE_ADDR'] . " " . date("[d/m/Y:H:i:s]") . " Username and/or password invalid. Login: " . $_SERVER['PHP_AUTH_USER'] . " Password: " . $_SERVER['PHP_AUTH_PW'] . "\n";
+            DebMes($data, 'auth');
             exit;
-         }
-      }
-   }
+        }
+    }
 }
 
 if (isset($_SERVER['REQUEST_METHOD']))
