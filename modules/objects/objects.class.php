5e04c071f90f9ab35977bf8444973a74fad639b9
tarasfrompir <tarasfrompir@inbox.ru>
2020-05-02 08:40:42 +0300

    Add files via upload

--- modules/objects/objects.class.php
+++ modules/objects/objects.class.php
@@ -849,27 +849,23 @@ class objects extends module
         if ($id) {
             $prop = SQLSelectOne("SELECT * FROM properties WHERE ID='" . $id . "'");
 
-            if ($prop['VALIDATION_TYPE'] == 1) {
-                if (!is_numeric($value)) return false;
-                if ($prop['VALIDATION_NUM_MIN'] != '' && (float)$value < (float)$prop['VALIDATION_NUM_MIN']) {
-                    return false;
-                }
-                if ($prop['VALIDATION_NUM_MAX'] != '' && (float)$value > (float)$prop['VALIDATION_NUM_MAX']) {
-                    return false;
-                }
-            }
-            if ($prop['VALIDATION_TYPE'] == 2) {
-                if ($value != '1' && $value != '0') {
-                    return false;
-                }
-            }
-            if ($prop['VALIDATION_TYPE'] == 3) {
-                $items = explode(',', $prop['VALIDATION_LIST']);
-                if (!in_array(mb_strtolower($value, 'UTF-8'), $items)) return false;
-            }
-            if ($prop['VALIDATION_TYPE'] == 100) {
-                eval($prop['VALIDATION_CODE']);
-                if (is_null($value)) return false;
+            switch ($prop['VALIDATION_TYPE']) {
+                case 1:
+                    if (!is_numeric($value)) return false;
+                    if ($prop['VALIDATION_NUM_MIN'] != '' && (float)$value < (float)$prop['VALIDATION_NUM_MIN']) return false;
+                    if ($prop['VALIDATION_NUM_MAX'] != '' && (float)$value > (float)$prop['VALIDATION_NUM_MAX']) return false;
+                    break;
+                case 2:
+                    if ($value != '1' && $value != '0') return false;
+                    break;
+                case 3:
+                    $items = explode(',', $prop['VALIDATION_LIST']);
+                    if (!in_array(mb_strtolower($value, 'UTF-8'), $items)) return false;
+                    break;
+                case 100:
+                    eval($prop['VALIDATION_CODE']);
+                    if (is_null($value)) return false;
+                    break;
             }
 
             $property = $prop['TITLE'];
@@ -1069,17 +1065,17 @@ class objects extends module
     }
 
     /**
-     * Objects subscription events
+     * terminals subscription events
      *
      * @access public
      */
     function processSubscription($event, $details = '') {
         if ($event == 'DAILY') {
             // почистим кеш 
-            SQLExec("DELETE FROM cached_values WHERE EXPIRE < NOW()");
-        }
+	        SQLExec("DELETE FROM cached_values WHERE EXPIRE < NOW()");
+		}
+ 
     }
-
     /**
      * Install
      *
