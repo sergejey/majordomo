5c22411d45bd96e684d6ab96d48ebc91859fd2a0
tarasfrompir <tarasfrompir@inbox.ru>
2020-05-13 09:16:43 +0300

    Update objects.class.php

--- lib/objects.class.php
+++ lib/objects.class.php
@@ -601,7 +601,7 @@ function getGlobal($varname)
         $object_name = 'ThisComputer';
     }
 
-    if (defined('SETTINGS_SYSTEM_USE_CASHE') && SETTINGS_SYSTEM_USE_CASHE && false !== $value = checkFromCache('MJD:' . $object_name . '.' . $property) ) {
+    if (defined('SYSTEM_DISABLE_CACHE') && SYSTEM_DISABLE_CACHE && false !== $value = checkFromCache('MJD:' . $object_name . '.' . $property) ) {
         return $value;
     }
 
