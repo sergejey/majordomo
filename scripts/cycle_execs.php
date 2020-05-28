57da29c47bb55e7bd07bfdcfa6bbc7183364cc3d
tarasfrompir <tarasfrompir@inbox.ru>
2020-05-28 08:41:58 +0300

    Update cycle_execs.php

--- scripts/cycle_execs.php
+++ scripts/cycle_execs.php
@@ -18,7 +18,7 @@ echo date("H:i:s") . " running " . basename(__FILE__) . "\n";
 SQLExec("DELETE FROM safe_execs");
 
 while (1) {
-    if (time() - $checked_time > 20) {
+    if (time() - $checked_time > 60) {
         $checked_time = time();
         setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
     }
