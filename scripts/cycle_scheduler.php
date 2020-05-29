31bf6565e4a73d0a61f080e66e42e36d0b9c54f2
tarasfrompir <tarasfrompir@inbox.ru>
2020-05-29 23:08:11 +0300

    Update cycle_scheduler.php

--- scripts/cycle_scheduler.php
+++ scripts/cycle_scheduler.php
@@ -16,13 +16,15 @@ include_once(DIR_MODULES . 'scripts/scripts.class.php');
 $ctl = new control_modules();
 $sc = new scripts();
 $checked_time = 0;
-
+setGlobal((str_replace('.php', '', basename(__FILE__))).'Run', time(), 1);
+$cycleVarName='ThisComputer.'.str_replace('.php', '', basename(__FILE__)).'Run';
 echo date("H:i:s") . " running " . basename(__FILE__) . PHP_EOL;
 
 while (1) {
-   if ((time()-$checked_time)>5) {
-      $checked_time = time();
-      setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+   if (time() - $checked_time > 30) {
+        //setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+        saveToCache("MJD:$cycleVarName", $checked_time);
+        $checked_time = time();
    }
    runScheduledJobs();
    $sc->checkScheduledScripts();
