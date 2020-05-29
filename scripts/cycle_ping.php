580fd9e538e5f061e10e58de16c37eb28954f706
tarasfrompir <tarasfrompir@inbox.ru>
2020-05-29 23:59:48 +0300

    Update cycle_ping.php

--- scripts/cycle_ping.php
+++ scripts/cycle_ping.php
@@ -29,10 +29,13 @@ while (1)
     if (time() - $checked_time > 30) {
         //setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
         saveToCache("MJD:$cycleVarName", $checked_time);
-        $checked_time = time();
    }
-   // checking all hosts
-   $pinghosts->checkAllHosts();
+    if (time() - $checked_time > 15) {
+        $checked_time = time();
+       //setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
+       // checking all hosts
+       $pinghosts->checkAllHosts();
+   }
    if (file_exists('./reboot') || IsSet($_GET['onetime']))
    {
       exit;
