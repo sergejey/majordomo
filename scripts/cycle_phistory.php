4f4c25060f62a1d678fbe4732c0b12d83863a1e0
london89 <london89@mail.ru>
2020-05-29 10:41:21 +0300

    Update cycle_phistory.php

--- scripts/cycle_phistory.php
+++ scripts/cycle_phistory.php
@@ -36,7 +36,7 @@ while (1) {
     if (time() - $checked_time > 5) {
         $checked_time = time();
 //        setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
-        SQLExec("REPLACE INTO cached_values (KEYWORD, DATAVALUE, EXPIRE)  VALUES ('MJD:$cycleVarName', '$checked_time','".date('Y-m-d H:i:s',$checked_time+60)."')");
+        saveToCache("MJD:$cycleVarName", $checked_time);
     }
 
 
