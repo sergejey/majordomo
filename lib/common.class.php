051ed08218e513143e7ca7fbc461dd9ffda61e76
Alexandr-logrus <nechaev.alex@gmail.com>
2020-07-08 20:13:52 +0500

    events.class.php
    1. вынес функции ивентов в отдельный файл
    2. добавил функцию registeredEventDetails чтение деталей ивента

--- lib/common.class.php
+++ lib/common.class.php
@@ -547,44 +547,6 @@ function recognizeTime($text, &$newText)
     return $result;
 }
 
-
-/**
- * Summary of registerEvent
- * @param mixed $eventName Event name
- * @param mixed $details Details (default '')
- * @param mixed $expire_in Expire time (default 365)
- * @return mixed
- */
-function registerEvent($eventName, $details = '', $expire_in = 0)
-{
-    include_once(DIR_MODULES . 'events/events.class.php');
-    $events = new events();
-    return $events->registerEvent($eventName, $details, $expire_in);
-}
-
-/**
- * Summary of registeredEventTime
- * @param mixed $eventName Event name
- * @return mixed
- */
-function registeredEventTime($eventName)
-{
-    $sqlQuery = "SELECT ID, UNIX_TIMESTAMP(ADDED) AS TM
-                  FROM events
-                 WHERE EVENT_TYPE = 'system'
-                   AND EVENT_NAME = '" . DBSafe($eventName) . "'
-                 ORDER BY ADDED DESC
-                 LIMIT 1";
-
-    $rec = SQLSelectOne($sqlQuery);
-
-    if ($rec['ID']) {
-        return $rec['TM'];
-    } else {
-        return -1;
-    }
-}
-
 /**
  * Summary of getRandomLine
  * @param mixed $filename File name
