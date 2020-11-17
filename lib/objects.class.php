c2ee38d2870dea65e908e862a7964299444de84f
tarasfrompir <tarasfrompir@inbox.ru>
2020-11-17 12:11:18 +0200

    Update objects.class.php

--- lib/objects.class.php
+++ lib/objects.class.php
@@ -273,7 +273,7 @@ function removeClassProperty($class_name, $property_name)
     $class_id = addClass($class_name);
 
     if ($class_id) {
-		SQLExec("DELETE FROM properties WHERE TITLE = '" . DBSafe($property_name) . "' AND OBJECT_ID = 0 AND CLASS_ID  = '" . $class_id . "'")
+		SQLExec("DELETE * FROM properties WHERE TITLE = '" . DBSafe($property_name) . "' AND OBJECT_ID = 0 AND CLASS_ID  = '" . $class_id . "'");
    		return true;
     }
     return false;
