d7f17f83222a37813e7be0ab2534719fdfdf0eb5
london89 <london89@mail.ru>
2021-01-28 15:30:43 +0300

    Update homebridgeSync.inc.php
    Adding temphum sensors

--- modules/devices/homebridgeSync.inc.php
+++ modules/devices/homebridgeSync.inc.php
@@ -113,6 +113,29 @@ for ($i = 0; $i < $total; $i++) {
             $payload['value'] = gg($devices[$i]['LINKED_OBJECT'] . '.value');
             sg('HomeBridge.to_set', json_encode($payload));
             break;
+        case 'sensor_temphum':
+            // Temp
+            $payload['service'] = 'TemperatureSensor';
+            $payload['CurrentTemperature']['minValue'] = -40;
+            sg('HomeBridge.to_add', json_encode($payload));
+
+            $payload['characteristic'] = 'CurrentTemperature';
+            $payload['value'] = gg($devices[$i]['LINKED_OBJECT'] . '.value');
+            sg('HomeBridge.to_set', json_encode($payload));
+
+            // Hum
+            $payload['name'] .= '_Hum';
+            $payload['service_name'] .= '_Hum';
+            $payload['service'] = 'HumiditySensor';
+            unset($payload['CurrentTemperature']['minValue']);
+            sg('HomeBridge.to_add', json_encode($payload));
+
+            $payload['characteristic'] = 'CurrentRelativeHumidity';
+            $payload['value'] = gg($devices[$i]['LINKED_OBJECT'] . '.valueHumidity');
+            sg('HomeBridge.to_set', json_encode($payload));
+
+            break;
+
         case 'motion':
             $payload['service'] = 'MotionSensor';
             sg('HomeBridge.to_add', json_encode($payload));
