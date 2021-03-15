70e2d9691a7442b846a8d06c3b11df5d71e62a93
Alexandr-logrus <nechaev.alex@gmail.com>
2021-03-15 16:15:14 +0500

    SCounters

--- modules/devices/SCounters_valueUpdated.php
+++ modules/devices/SCounters_valueUpdated.php
@@ -1,91 +1,39 @@
 <?php
 
-$ot=$this->object_title;
+$ot = $params['ORIGINAL_OBJECT_TITLE'];
+$new_value = (float)$params['NEW_VALUE'];
+$old_value = (float)$params['OLD_VALUE'];
 
 $this->callMethodSafe('keepAlive');
 $this->callMethodSafe('statusUpdated');
 $this->callMethodSafe('logicAction');
 
-$linked_room=$this->getProperty('linkedRoom');
-
-if (!isset($params['NEW_VALUE'])) {
-    $new_value=(float)$this->getProperty('value');
-} else {
-    $new_value=(float)$params['NEW_VALUE'];
-}
-
-
-
-$history_values=array(
-    'valueHour'=>date('Y-m-d H:00:00'),
-    'valueDay'=>date('Y-m-d 00:00:00')
+$history_values = array(
+ 'valueHour'=>date('Y-m-d H:00:00'),
+ 'valueDay'=>date('Y-m-d 00:00:00'),
+ 'valueMonth'=>date('Y-m-01 00:00:00')
 );
-//,'valueMonth'=>date('Y-m-01 00:00:00')
 
-$main_value_id = getHistoryValueId($ot.'.value');
-if (defined('SEPARATE_HISTORY_STORAGE') && SEPARATE_HISTORY_STORAGE == 1) {
-    $main_table_name = createHistoryTable($main_value_id);
-} else {
-    $main_table_name = 'phistory';
+foreach ($history_values as $history_value=>$time) {
+ $value_id = (int)getHistoryValueId($ot . '.' . $history_value);
+ if (defined('SEPARATE_HISTORY_STORAGE') && SEPARATE_HISTORY_STORAGE == 1) {
+  $table_name = createHistoryTable($value_id);
+ } else {
+  $table_name = 'phistory';
+ }
+ $val1 = SQLSelectOne("SELECT ID, VALUE FROM pvalues WHERE ID='" . $value_id . "' AND UPDATED>=('" . $time . "')");
+ if ($val1['ID']) {
+  SQLExec("DELETE FROM $table_name WHERE VALUE_ID=$value_id AND ADDED>=('" . $time . "')");
+  $set_value = $val1['VALUE'] + ($new_value - $old_value);
+  $set_value = round($set_value, 2);
+  $this->setProperty($history_value, $set_value);
+ } else {
+  $set_value = $new_value - $old_value;
+  $set_value = round($set_value, 2);
+  $this->setProperty($history_value, $set_value);
+  //DebMes($history_value . ' ' . $time . ' - id - ' . $value_id .  ' - new - ' . $new_value . ' - old - ' . $old_value, $ot);
+ }
 }
-
-$prev_value_set = 0;
-foreach($history_values as $history_value=>$time) {
-    $val1 = SQLSelectOne("SELECT ID, VALUE FROM $main_table_name WHERE VALUE_ID='".$main_value_id."' AND ADDED>=('".$time."') ORDER BY ADDED LIMIT 1");
-    $prev_value = 0;
-    if ($val1['ID']) {
-        $prev_value=$val1['VALUE'];
-        $set_value = $new_value-$prev_value;
-    } else {
-        $set_value = 0;
-    }
-    $set_value = round($set_value,2);
-    $value_id = (int)getHistoryValueId($ot.'.'.$history_value);
-    if (defined('SEPARATE_HISTORY_STORAGE') && SEPARATE_HISTORY_STORAGE == 1) {
-        $table_name = createHistoryTable($value_id);
-    } else {
-        $table_name = 'phistory';
-    }
-    SQLExec("DELETE FROM $table_name WHERE VALUE_ID=$value_id AND ADDED>=('".$time."')");
-    //echo "Setting $history_value to $set_value<br/>";
-    $this->setProperty($history_value,$set_value);
-}
-
-// DAY
-
-// MONTH
-//sum of days
-$time=date('Y-m-01 00:00:00');
-$time2=date('Y-m-d 00:00:00');
-$history_value='valueDay';
-$value_id = (int)getHistoryValueId($ot.'.'.$history_value);
-if (defined('SEPARATE_HISTORY_STORAGE') && SEPARATE_HISTORY_STORAGE == 1) {
-    $table_name = createHistoryTable($value_id);
-} else {
-    $table_name = 'phistory';
-}
-
-$byDays = SQLSelect("SELECT MAX(VALUE) as DAY_TOTAL FROM $table_name WHERE VALUE_ID='".$value_id."' AND ADDED>=('".$time."') AND ADDED<('".$time2."') GROUP BY TO_DAYS(ADDED)");
-$total = count($byDays);
-$monthTotal=0;
-for($i=0;$i<$total;$i++) {
-    $monthTotal+=$byDays[$i]['DAY_TOTAL'];
-}
-/*
-$val1 = SQLSelectOne("SELECT SUM(VALUE) as TOTAL FROM $table_name WHERE VALUE_ID='".$value_id."' AND ADDED>=('".$time."') AND ADDED<('".$time2."')");
-$set_value = round($val1['TOTAL']+$this->getProperty('valueDay'),2);
-*/
-$set_value = round($monthTotal+$this->getProperty('valueDay'),2);
-$history_value='valueMonth';
-$value_id = (int)getHistoryValueId($ot.'.'.$history_value);
-if (defined('SEPARATE_HISTORY_STORAGE') && SEPARATE_HISTORY_STORAGE == 1) {
-    $table_name = createHistoryTable($value_id);
-} else {
-    $table_name = 'phistory';
-}
-SQLExec("DELETE FROM $table_name WHERE VALUE_ID=$value_id AND ADDED>=('".$time."')");
-$this->setProperty($history_value,$set_value);
-
 
 include_once(dirname(__FILE__).'/devices.class.php');
 $dv=new devices();
