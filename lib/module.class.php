6b32ec21aa54e35a51300742f2ff52f5f9f02905
tarasfrompir <tarasfrompir@inbox.ru>
2021-06-20 11:15:24 +0300

    Update module.class.php

--- lib/module.class.php
+++ lib/module.class.php
@@ -541,7 +541,7 @@ class module
             $queryCnt = count($query) - 1;
 
             for ($i = 0; $i < $queryCnt; $i++) {
-                if ($query[$i]{0} != "#") {
+                if ($query[$i][0] != "#") {
                     SQLExec($query[$i]);
                     $mdf[] = "#" . $query[$i];
                 } else {
