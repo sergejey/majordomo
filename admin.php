bb877463eb5bc33f16d7ef9947fbda74b3a85031
tarasfrompir <tarasfrompir@inbox.ru>
2021-07-09 20:10:43 +0300

    Update admin.php
    Как хотите так и делайте, но дело в том что даже функция такая отсутствует в этом классе - restoreParams()

--- admin.php
+++ admin.php
@@ -25,10 +25,7 @@ include_once(DIR_MODULES . "control_modules/control_modules.class.php");
 $cl  = new control_modules();
 $app = new panel();
 
-if ($md != $app->name)
-   $app->restoreParams();
-else
-   $app->getParams();
+$app->getParams();
 
 startMeasure('apprun');
 $result = $app->run();
