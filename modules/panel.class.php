af3b463a8eaf42b7a57af02ea53ce9405a0d6f11
tarasfrompir <tarasfrompir@inbox.ru>
2021-07-11 07:28:39 +0300

    Update panel.class.php

--- modules/panel.class.php
+++ modules/panel.class.php
@@ -16,6 +16,7 @@ class panel extends module
     function __construct()
     {
         $this->name = "panel";
+        $this->print = null;
     }
 
 // --------------------------------------------------------------------
