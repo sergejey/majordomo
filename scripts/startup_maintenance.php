fe75b25cef53df9f0953720fba10f8c0f48b680d
tarasfrompir <tarasfrompir@inbox.ru>
2021-08-11 13:03:30 +0300

    Update startup_maintenance.php

--- scripts/startup_maintenance.php
+++ scripts/startup_maintenance.php
@@ -1,14 +1,4 @@
 <?php
-
-/*
-chdir('../');
-include_once("./config.php");
-include_once("./lib/loader.php");
-
-
-$db=new mysql(DB_HOST, '', DB_USER, DB_PASSWORD, DB_NAME); // connecting to database
-include_once("./load_settings.php");
-*/
 /*
  * @version 0.1 (auto-set)
  */
