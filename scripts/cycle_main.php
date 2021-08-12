acc0f9f8dcc7bb8a762a6f1ba08b5057b74b0b7e
tarasfrompir <tarasfrompir@inbox.ru>
2021-08-12 19:15:20 +0300

    Update cycle_main.php

--- scripts/cycle_main.php
+++ scripts/cycle_main.php
@@ -4,14 +4,10 @@ chdir(dirname(__FILE__) . '/../');
 
 include_once("./config.php");
 include_once("./lib/loader.php");
-include_once("./lib/threads.php");
 
 set_time_limit(0);
 
 include_once("./load_settings.php");
-include_once(DIR_MODULES . "control_modules/control_modules.class.php");
-
-$ctl = new control_modules();
 
 setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
 $cycleVarName = 'ThisComputer.' . str_replace('.php', '', basename(__FILE__)) . 'Run';
