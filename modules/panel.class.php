da32237aedfda171ecfea422a0529874e166af79
tarasfrompir <tarasfrompir@inbox.ru>
2021-09-03 16:25:10 +0300

    Update panel.class.php

--- modules/panel.class.php
+++ modules/panel.class.php
@@ -234,7 +234,7 @@ class panel extends module
         }
 
         $out["ACTION"] = $this->action;
-        if (!$out['TITLE']) {
+        if (!isset($out['TITLE'])) {
             $out['TITLE'] = LANG_CONTROL_PANEL;
         }
         $this->data = $out;
