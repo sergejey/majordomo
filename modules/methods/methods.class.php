d6cf6f6b03805ff2cf63091d353199c586f55c1b
tarasfrompir <tarasfrompir@inbox.ru>
2021-09-09 13:14:09 +0300

    Update methods.class.php

--- modules/methods/methods.class.php
+++ modules/methods/methods.class.php
@@ -11,6 +11,14 @@
 //
 //
 class methods extends module {
+  var $data_source;
+  var $view_mode;
+  var $edit_mode;
+  var $mode;
+  var $tab;
+  var $single_rec;
+  var $ajax;
+  
 /**
 * methods
 *
