7548514db47cb5c230e7d113b650381f6a564f15
Sergei Jeihala <sergejey@gmail.com>
2023-02-07 18:01:18 +0200

    Update SDimmers_levelUpdated.php
    Dimmer level fix

--- modules/devices/SDimmers_levelUpdated.php
+++ modules/devices/SDimmers_levelUpdated.php
@@ -1,17 +1,9 @@
 <?php
 
-/*
- $tm=time();
- $this->setProperty('updated', $tm);
- $this->setProperty('updatedText', date('H:i', $tm));
-*/
-
 $level = $this->getProperty('level');
 $minWork = $this->getProperty('minWork');
 $maxWork = $this->getProperty('maxWork');
 $levelWork = $this->getProperty('levelWork');
-
-//DebMes("Level updated to " . $level, 'dimming');
 
 $statusUpdated = 0;
 
@@ -21,23 +13,20 @@ if ($level > 0) {
         $statusUpdated = 1;
         $this->setProperty('status', 1, false);
     }
-    if ($minWork != $maxWork) {
-        //DebMes("Level updated to " . $level ." ".$_SERVER['REQUEST_URI'], 'dimming');
-        $levelWork = round($minWork + round(($maxWork - $minWork) * $level / 100));
-        if ($this->getProperty('levelWork') != $levelWork) {
-            //DebMes("Setting new levelWork to " . (int)$levelWork, 'dimming');
-            $this->setProperty('levelWork', (int)$levelWork);
-        }
-    }
 } else {
     if ($this->getProperty('status')) {
         $statusUpdated = 1;
         $this->setProperty('status', 0);
     }
-    if ($levelWork>0) {
-        $this->setProperty('levelWork', 0);
+}
+
+if ($minWork != $maxWork) {
+    $levelWork = round($minWork + round(($maxWork - $minWork) * $level / 100));
+    if ($this->getProperty('levelWork') != $levelWork) {
+        $this->setProperty('levelWork', (int)$levelWork);
     }
 }
+
 
 if (!$statusUpdated) {
     $this->callMethod('logicAction');
