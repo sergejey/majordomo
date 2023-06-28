3acd3bfe80be04520f2a1939d30795835aa9843f
Александр <56637712+whitevast@users.noreply.github.com>
2023-06-28 13:05:12 +0800

    Update devices_links_actions.inc.php

--- modules/devices/devices_links_actions.inc.php
+++ modules/devices/devices_links_actions.inc.php
@@ -148,6 +148,18 @@ if (!$device1['SYSTEM_DEVICE'] && !$device1['ARCHIVED'] && $this->isHomeBridgeAv
                 }
             }
             break;
+        case 'unlockable':
+			$payload['service'] = 'LockMechanism';
+			$payload['characteristic'] = 'LockCurrentState';
+			$payload['value'] = (int)gg($device1['LINKED_OBJECT'] . '.lockstatus');
+			sg('HomeBridge.to_set', json_encode($payload));
+			$payload['name'] .= "_sensor";
+			$payload['service_name'] .= "_sensor";
+            $payload['service'] = 'ContactSensor';
+            $payload['characteristic'] = 'ContactSensorState';
+            $nc = gg($device1['LINKED_OBJECT'] . '.ncno') == 'nc';
+            $payload['value'] = $nc ? 1 - gg($device1['LINKED_OBJECT'] . '.status') : gg($device1['LINKED_OBJECT'] . '.status');
+            break;
         case 'rgb':
             $payload['service'] = 'Lightbulb';
             sg('HomeBridge.to_add', json_encode($payload));
