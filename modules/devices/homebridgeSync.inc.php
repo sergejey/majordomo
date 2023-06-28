8f77e8743bdf7831a79ec53809acefde8e57e1ee
Александр <56637712+whitevast@users.noreply.github.com>
2023-06-28 13:23:01 +0800

    Update homebridgeSync.inc.php

--- modules/devices/homebridgeSync.inc.php
+++ modules/devices/homebridgeSync.inc.php
@@ -99,21 +99,20 @@ for ($i = 0; $i < $total; $i++) {
             }
             break;
         case 'unlockable':
-			//Замок
-			$payload['service'] = 'LockMechanism';
-			$payload['characteristic'] = 'LockCurrentState';
+	    //Замок
+	    $payload['service'] = 'LockMechanism';
+	    $payload['characteristic'] = 'LockCurrentState';
             sg('HomeBridge.to_add', json_encode($payload));
-			$payload['value'] = (int)gg($devices[$i]['LINKED_OBJECT'] . '.lockstatus');
-			sg('HomeBridge.to_set', json_encode($payload));
-			//Датчик закрытия
-			$payload['name'] .= "_sensor";
-			$payload['service_name'] .= "_sensor";
+	    $payload['value'] = (int)gg($devices[$i]['LINKED_OBJECT'] . '.lockstatus');
+	    sg('HomeBridge.to_set', json_encode($payload));
+	    //Датчик закрытия
+	    $payload['name'] .= "_sensor";
+	    $payload['service_name'] .= "_sensor";
             $payload['service'] = 'ContactSensor';
             sg('HomeBridge.to_add', json_encode($payload));
             $payload['characteristic'] = 'ContactSensorState';
             $payload['value'] = (int)gg($devices[$i]['LINKED_OBJECT'] . '.ncno') == 'nc' ? 1 - gg($devices[$i]['LINKED_OBJECT'] . '.status') : gg($devices[$i]['LINKED_OBJECT'] . '.status');
             sg('HomeBridge.to_set', json_encode($payload));
-			debmes($payload, 'homebridge');
             break;
         case 'sensor_temp':
             $payload['service'] = 'TemperatureSensor';
