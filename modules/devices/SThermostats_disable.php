262d92af47bcaa2ede026b1cdcb1a68ba27a96a6
Sergei Jeihala <sergejey@gmail.com>
2023-10-07 12:35:51 +0300

    Thermostat update
    * Simple devices - thermostat logic adjustment

--- modules/devices/SThermostats_disable.php
+++ modules/devices/SThermostats_disable.php
@@ -1,4 +1,10 @@
 <?php
 
 $this->setProperty('disabled', 1);
-$this->setProperty('relay_status', 0); // turn off
+$this->setProperty('relay_status', 0); // turn off relay
+
+$currentTemperature = $this->getProperty('value');
+include_once(dirname(__FILE__).'/devices.class.php');
+$dv=new devices();
+$dv->checkLinkedDevicesAction($this->object_title, $currentTemperature);
+$this->callMethod('logicAction');
